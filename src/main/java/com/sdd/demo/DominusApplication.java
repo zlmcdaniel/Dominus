68a0c46fa73ec96df6f175a018567e1ed17caeca
Ashwin Chitoor <ashketchup55@gmail.com>
2020-12-07 17:10:35 -0500

    Initial

---
+++ src/main/java/com/sdd/demo/DominusApplication.java
@@ -0,0 +1,13 @@
+package com.sdd.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DominusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DominusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sdd/demo/DominusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdd.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DominusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
