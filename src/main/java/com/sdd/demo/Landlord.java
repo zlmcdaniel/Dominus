3386cc867c8fe734f6470f2229e445f6bc6311cb
Shardul Joshi <joshardul@gmail.com>
2020-12-08 18:54:10 -0500

    Landlord class first pass
    Simple Landlord class extending User class

---
+++ src/main/java/com/sdd/demo/Landlord.java
@@ -0,0 +1,29 @@
+// Java library import(s)
+import java.util.List;
+
+public class Landlord extends User {
+	
+	// Representation
+	private ArrayList<Review> reviews;
+	// Remaining representation inherited from User
+
+	// Constructor(s)
+	public Landlord( String first_name_, String last_name_ ) {
+		// Set up user
+		super( null, first_name_, last_name_, null, null );
+
+		// Create empty list for reviews
+		reviews = new ArrayList<Review>();
+	}
+
+	// Accessor(s)
+	public ArrayList<Review> getReviews() {
+		return reviews;
+	}
+
+	// Modifiers(s)
+	public boolean addReview( Review review ) {
+		return reviews.add( review );
+	}
+
+}
