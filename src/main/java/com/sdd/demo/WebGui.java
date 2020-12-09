5495b0b34490e12510b76747287ef6f98ae5600d
Ashwin Chitoor <ashketchup55@gmail.com>
2020-12-09 16:44:09 -0500

    Create WebGui.java

---
+++ src/main/java/com/sdd/demo/WebGui.java
@@ -0,0 +1,25 @@
+public class WebGui {
+    void displayReviews( Landlord landlord ) {
+        System.out.println( landlord.getReviews() );
+    }
+    void displayProfile( User user ) {
+        System.out.println( user );
+    }
+    void addAccount( String username, String first_name, 
+                     String last_name, String email, 
+                     String password ) {
+        Controller.handleEvent( "addAccount", username, first_name, last_name, email, password );
+    }
+    boolean login( String username, String password ) {
+        Controller.handleEvent( "login", username, password );
+    }
+    void viewStrikes( Renter renter ) {
+        System.out.print( renter.getStrikes() );
+    }
+    boolean addReview( String title, String body, int num_stars) {
+        Controller.handleEvent( "addReview", title, body, num_stars );
+    }
+    void removeReview( String id ) {
+        Controller.removeReview( "removeReview", id );
+    }
+}
