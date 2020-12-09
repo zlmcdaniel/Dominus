a00db5c344e369de02229db2c13161e78ef77168
zlmcdaniel <zlmcdaniel@gmail.com>
2020-12-09 16:42:58 -0500

    Updated review/user systems

--- src/main/java/com/sdd/demo/ReviewSystem.java
+++ src/main/java/com/sdd/demo/ReviewSystem.java
@@ -1,15 +1,55 @@
-package com.sdd.demo;
-
 import java.util.ArrayList;
 
 public class ReviewSystem {
-    boolean addReview( Review review ) {
-        return database.addReview( review );
+    
+    private ArrayList<Review> reviews;
+
+    public ReviewSystem() {
+        super();
+
+        reviews = new ArrayList<Review>();
     }
-    boolean removeReview( String id ) {
-        return database.removeReview( id );
+
+    // Add a Review to the System
+    public boolean addReview( Review review ) {
+        return reviews.add( review );
     }
-    ArrayList<Review> getReviews( String keyword ) {
-        return database.getReviews( keyword );
+
+    // Remove a Review to the System
+    public boolean removeReview( String id ) {
+        for( Review review : reviews )
+        {
+            if( review.getId().equals( id ) ) {
+                return reviews.remove( review );
+            }
+        }
+
+        return false;
     }
+
+    // Get all Reviews that apply to the given keyword
+    public ArrayList<Review> getReviews( String keyword ) {
+        ArrayList<Review> results = new ArrayList<Review>();
+
+        for( Review review : reviews )
+        {
+            if( review.getTitle().indexOf( keyword ) != -1 ) {
+                results.add( review );
+                continue;
+            }
+
+            else if( review.getComment().indexOf( keyword ) != -1 ) {
+                results.add( review );
+                continue;
+            }
+
+            else if( review.getReviewer().getFirstName().indexOf( keyword ) != -1 || review.getReviewer().getLastName().indexOf( keyword ) != -1) {
+                results.add( review );
+                continue;
+            }
+        }
+
+        return results;
+    }
+
 }
--- src/main/java/com/sdd/demo/UserSystem.java
+++ src/main/java/com/sdd/demo/UserSystem.java
@@ -1,16 +1,43 @@
-package com.sdd.demo;
+import java.util.ArrayList;
 
 public class UserSystem {
-    boolean addUser( User user ) {
-        return database.add( user );
+    
+    private ArrayList<User> users;
+
+    public UserSystem() {
+        super();
+
+        users = new ArrayList<User>();
     }
-    User getUserByUsername( String username ) {
-        return database.get( username );
+
+    // Add a user to the System
+    public boolean addUser( User user ) {
+        return users.add( user );
     }
-    int getStrikes( User user ) {
-        return database.get( user ).strikes;
-    } 
-    boolean authenticateUser( String username, String password ) {
-        return database.authenticate( username, password );
+
+    // Get a User by the username
+    public User getUserByUsername( String username ) {
+        for( User user : users )
+        {
+            if( user.getUsername().equals( username ) ) {
+                return user;
+            }
+        }
+
+        return null;
+    }
+
+    // Get the strikes for the user
+    public int getStrikes( User user ) {
+        return user.getStrikes();
+    }
+
+    // Check if the user is authenticated
+    public boolean authenticateUser( String username, String password ) {
+        if( getUserByUsername( username ) != null && getUserByUsername( username ).checkPassword( password ) ) {
+            return true;
+        }
+
+        return false;
     }
 }
