2704f6811bc96112c6436a5dec209b24cc157881
alexander-fetterman <afyankeefan@gmail.com>
2020-12-09 17:36:43 -0500

    Added StringMasher class

--- src/main/java/com/sdd/demo/WebGui.java
+++ src/main/java/com/sdd/demo/WebGui.java
@@ -17,18 +17,42 @@ public class WebGui {
     void addAccount( String username, String first_name, 
                      String last_name, String email, 
                      String password ) {
-        Controller.handleEvent( "addAccount", username, first_name, last_name, email, password );
+        Controller.handleEvent( StringMasher.mash("addAccount", username, first_name, last_name, email, password) );
     }
     boolean login( String username, String password ) {
-        Controller.handleEvent( "login", username, password );
+        try {
+            Controller.handleEvent(StringMasher.mash("login", username, password));
+            return true;
+        } catch (Exception e) {
+            java.lang.System.out.println( e.getMessage() );
+            return false;
+        }
     }
     void viewStrikes( Renter renter ) {
         java.lang.System.out.print( renter.getStrikes() );
     }
     boolean addReview( String title, String body, int num_stars) {
-        Controller.handleEvent( "addReview", title, body, num_stars );
+        try {
+            Controller.handleEvent( StringMasher.mash("addReview", title, body, num_stars) );
+            return true;
+        } catch (Exception e) {
+            java.lang.System.out.println( e.getMessage() );
+            return false;
+        }
+
     }
     void removeReview( String id ) {
-        Controller.removeReview( "removeReview", id );
+        Controller.handleEvent( StringMasher.mash( "removeReview", id ) );
+    }
+}
+
+class StringMasher {
+    public static String mash(Object... objs) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Object o : objs) {
+            stringBuilder.append(o);
+            stringBuilder.append(",");
+        }
+        return stringBuilder.toString();
     }
 }
