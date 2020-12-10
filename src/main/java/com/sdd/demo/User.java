ae40cfb816a6714d9a98dea369e3b6307124436b
alexander-fetterman <afyankeefan@gmail.com>
2020-12-10 18:01:02 -0500

    Added GUI stuff

--- src/main/java/com/sdd/demo/User.java
+++ src/main/java/com/sdd/demo/User.java
@@ -71,4 +71,50 @@ public abstract class User {
     public boolean checkPassword(String password) {
         return password.equals(this.password);
     }
+
+    /* Spring Methods */
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    private void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSecret_question() {
+        return secret_question;
+    }
+
+    public void setSecret_question(String secret_question) {
+        this.secret_question = secret_question;
+    }
+
+    private String getSecret_answer() {
+        return secret_answer;
+    }
+
+    public void setSecret_answer(String secret_answer) {
+        this.secret_answer = secret_answer;
+    }
 }
--- src/main/java/com/sdd/demo/WebController.java
+++ src/main/java/com/sdd/demo/WebController.java
@@ -3,13 +3,22 @@ package com.sdd.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WebController {
-    @GetMapping("/createaccount")
+    @GetMapping("/renter")
     public String createAccount( Model model ) {
+        model.addAttribute("renter", new Renter());
         return "createaccount";
+    }
+
+    @PostMapping("/renter")
+    public String createAccountSubmit(@ModelAttribute Renter renter, Model model) {
+        model.addAttribute("renter", renter);
+        return "newaccount";
     }
 
     @GetMapping("/user/Bob")
