7a78ac57191d1b8b7662588298b3627eb024e183
alexander-fetterman <afyankeefan@gmail.com>
2020-12-10 17:17:04 -0500

    Added WebController

--- src/main/java/com/sdd/demo/WebController.java
+++ src/main/java/com/sdd/demo/WebController.java
@@ -1,4 +1,14 @@
 package com.sdd.demo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class WebController {
+    @GetMapping("/createaccount")
+    public String greeting(Model model) {
+        return "createaccount";
+    }
 }
